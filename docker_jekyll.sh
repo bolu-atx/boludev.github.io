2afbaab213c4145ec8bde10060c8817cd08e5b82
Bo Lu <bo@plenoinc.com>
2022-09-18 09:07:21 -0700

    get jekyll to run in docker

---
+++ docker_jekyll.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export JEKYLL_VERSION=3.8
+
+which docker 2>&1 > /dev/null
+if [[ $? -ne 0 ]]; then
+  echo "No docker found. cannot continue"
+fi
+
+
+docker run --rm \
+  --volume="$PWD:/srv/jekyll:Z" \
+  --volume="$PWD/.bundle:/usr/local/bundle:Z" \
+  --platform linux/amd64 \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll "$@"
